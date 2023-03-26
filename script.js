a23ddc57af2675c98fc81c8bfc439c7489a395ff
saudngamer <128940164+saudngamer@users.noreply.github.com>
2023-03-26 03:09:02 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function saveCard() {
-    let name = document.getElementById("name").value;
-    let message = "عيد سعيد " + name + "!";
-    let container = document.querySelector(".container");
-    container.innerHTML = "<h1>" + message + "</h1>";
-    container.style.backgroundColor = "#FFD700";
-    container.style.color = "white";
-    container.style.textAlign = "center";
-}
